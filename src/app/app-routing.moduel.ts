bb6d9350b88fe57ba5c145c94acb0c0a1e3b53f8
thanadilok-11576 <thanadilok@aqua.co.th>
2023-12-24 21:20:48 +0700

    ng-content

---
+++ src/app/app-routing.moduel.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+
+const routes: Routes = [
+  {
+    path: 'elements',
+    loadChildren: async () => {
+      const m = await import('./elements/elements.module');
+      return m.ElementsModule;
+    }
+  },
+  {
+    path: 'collections',
+    loadChildren: async() => {
+      const m = await import('./collections/collections.module');
+      return m.CollectionsModule;
+    }
+  },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.moduel';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
